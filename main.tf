5cc9ed1657a3d6158878c582f6f7a640185f93bd
tu-nombre-de-usuario <braynermc2021@gmail.com>
2025-06-30 11:38:34 -0500

    APIS UNIFICADOS CON S3

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_security_group" "consumer_sg_v6" {
-  name        = "consumer-sg-v6"
+resource "aws_security_group" "consumer_sg_bra" {
+  name        = "consumer-sg-bra"
   description = "Permite acceso HTTP y SSH"
 
   ingress {
@@ -26,8 +26,8 @@ resource "aws_security_group" "consumer_sg_v6" {
   }
 }
 
-resource "aws_iam_role" "ec2_consumer_role_v6" {
-  name = "EC2ConsumerS3ReadOnlyRolev6"
+resource "aws_iam_role" "ec2_consumer_role_bra" {
+  name = "EC2ConsumerS3ReadOnlyRolebra"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -38,23 +38,23 @@ resource "aws_iam_role" "ec2_consumer_role_v6" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "s3_read_v6" {
-  role       = aws_iam_role.ec2_consumer_role_v6.name
+resource "aws_iam_role_policy_attachment" "s3_read_bra" {
+  role       = aws_iam_role.ec2_consumer_role_bra.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
-resource "aws_iam_instance_profile" "ec2_profile_v6" {
-  name = "EC2ConsumerProfilev6"
-  role = aws_iam_role.ec2_consumer_role_v6.name
+resource "aws_iam_instance_profile" "ec2_profile_bra" {
+  name = "EC2ConsumerProfilebra"
+  role = aws_iam_role.ec2_consumer_role_bra.name
 }
 
-resource "aws_instance" "consumer_v6" {
+resource "aws_instance" "consumer_bra" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.consumer_sg_v6.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile_v6.name
+  vpc_security_group_ids = [aws_security_group.consumer_sg_bra.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile_bra.name
   user_data              = file("${path.module}/scripts/setup.sh")
 
-  tags = { Name = "EC2-Consumer-v6" }
+  tags = { Name = "EC2-Consumer-bra" }
 }
