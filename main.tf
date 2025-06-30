8b2eaefe5fc646570e5209fb0366fec23b94cb89
tu-nombre-de-usuario <braynermc2021@gmail.com>
2025-06-30 00:04:12 -0500

    EC2 prueba

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_security_group" "consumer_sg_v2" {
-  name        = "consumer-sg-v2"
+resource "aws_security_group" "consumer_sg_v3" {
+  name        = "consumer-sg-v3"
   description = "Permite acceso HTTP y SSH"
 
   ingress {
@@ -26,8 +26,8 @@ resource "aws_security_group" "consumer_sg_v2" {
   }
 }
 
-resource "aws_iam_role" "ec2_consumer_role_v2" {
-  name = "EC2ConsumerS3ReadOnlyRoleV2"
+resource "aws_iam_role" "ec2_consumer_role_v3" {
+  name = "EC2ConsumerS3ReadOnlyRoleV3"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -38,23 +38,23 @@ resource "aws_iam_role" "ec2_consumer_role_v2" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "s3_read_v2" {
-  role       = aws_iam_role.ec2_consumer_role_v2.name
+resource "aws_iam_role_policy_attachment" "s3_read_v3" {
+  role       = aws_iam_role.ec2_consumer_role_v3.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
-resource "aws_iam_instance_profile" "ec2_profile_v2" {
-  name = "EC2ConsumerProfileV2"
-  role = aws_iam_role.ec2_consumer_role_v2.name
+resource "aws_iam_instance_profile" "ec2_profile_v3" {
+  name = "EC2ConsumerProfileV3"
+  role = aws_iam_role.ec2_consumer_role_v3.name
 }
 
-resource "aws_instance" "consumer_v2" {
+resource "aws_instance" "consumer_v3" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.consumer_sg_v2.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile_v2.name
+  vpc_security_group_ids = [aws_security_group.consumer_sg_v3.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile_v3.name
   user_data              = file("${path.module}/scripts/setup.sh")
 
-  tags = { Name = "EC2-Consumer-V2" }
+  tags = { Name = "EC2-Consumer-V3" }
 }
