317a70c47600f13cbd31322bfa2080729cc4e6a7
tu-nombre-de-usuario <braynermc2021@gmail.com>
2025-06-30 00:05:37 -0500

    EC2 prueba

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_consumer_ip" {
-  description = "IP pública de la instancia EC2 Consumer V2"
-  value       = aws_instance.consumer_v2.public_ip
+  description = "IP pública de la instancia EC2 Consumer V3"
+  value       = aws_instance.consumer_v3.public_ip
 }
